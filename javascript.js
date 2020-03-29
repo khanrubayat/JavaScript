de97e7e155d64fbd635ee1f9aaecb1d1f2baffde
Nusrat Songita Khan <33320507+khanrubayat@users.noreply.github.com>
2020-03-29 20:18:31 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var show = document.getElementById("label1");
+ var showAgain = document.getElementsByClassName("showTime");
+function fun(){
+   show.innerHTML=Date();
+    showAgain[0].innerHTML=Date();
+}
